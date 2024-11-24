6cbb10ac10bb44bbac48b892ee22852a3d46d08e
CarlaFerreira19 <carlaferreira1919a@gmail.com>
2024-11-24 01:40:22 -0300

    Add files via upload

---
+++ AtividadeReact-Nana/src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/home";
+import Ativ1 from "./pages/atividade1/ativ1";
+import Ativ2 from "./pages/atividade2/ativ2";
+import Ativ3 from "./pages/atividade3/Atv03";
+import Ativ4 from "./pages/atividade4/Atv04";
+import Ativ5 from "./pages/atividade5/atv05";
+import Navbar from "./Layout/navbar";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/atividade1" element={<Ativ1 />} />
+        <Route path="/atividade2" element={<Ativ2 />} />
+        <Route path="/atividade3" element={<Ativ3 />} />
+        <Route path="/atividade4" element={<Ativ4 />} />
+        <Route path="/atividade5" element={<Ativ5 />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ AtividadeReact-Nana/src/Layout/navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./navbar.scss";
+
+const Navbar = () => {
+  return (
+    <ul className="menu cf">
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/atividade1">Semana 1</Link>
+      </li>
+      <li>
+        <Link to="/atividade2">Semana 2</Link>
+      </li>
+      <li>
+        <Link to="/atividade3">Semana 3</Link>
+      </li>
+      <li>
+        <Link to="/atividade4">Semana 4</Link>
+      </li>
+      <li>
+        <Link to="/atividade5">Semana 5</Link>
+      </li>
+    </ul>
+  );
+};
+
+export default Navbar;
